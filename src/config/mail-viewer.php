e79869a88e6851267a32f77258e2ca7fa0542810
Roman Igoshin <igoshin18@gmail.com>
2018-05-23 10:22:03 +0300

    replace mail-viewer uri with _mail-viewer with no ability to cahnge

--- src/config/mail-viewer.php
+++ src/config/mail-viewer.php
@@ -2,7 +2,6 @@
 
 return [
 	'table'           => 'mail_logs',
-	'uri_prefix'      => '_mail-viewer',
 	'middleware'      => ['web'],
 	'date_format'     => 'd.m.Y H:i:s',
 	'emails_per_page' => 20,
--- src/resources/assets/js/app.js
+++ src/resources/assets/js/app.js
@@ -1,73 +1,73 @@
 require('./bootstrap');
 
 window.app = new Vue({
-    el: '#app',
+  el: '#app',
 
-    data() {
-        return {
-            firstLoad: true,
-            loadingMails: false,
-            loadingMail: false,
-            mails: {
-                next_page_url: window.location.origin + '/' + this.$el.dataset.uri
-            },
-            currentMail: null,
-        };
+  data() {
+    return {
+      firstLoad: true,
+      loadingMails: false,
+      loadingMail: false,
+      mails: {
+        next_page_url: window.location.origin + '/_mail-viewer'
+      },
+      currentMail: null,
+    };
+  },
+
+  mounted() {
+    this.init();
+  },
+
+  methods: {
+    init() {
+      let vm = this;
+
+      vm.loadMails();
     },
 
-    mounted() {
-        this.init();
+    loadMails() {
+      let vm = this;
+
+      vm.loadingMails = true;
+      axios.get(vm.mails.next_page_url)
+        .then(response => {
+          let data = response.data;
+
+          if (data.success) {
+            let mails = vm.mails.data || [];
+
+            _.each(data.data.data, (mail) => {
+              mails.push(mail);
+            });
+
+            vm.mails = data.data;
+            vm.mails.data = mails;
+
+            if (vm.mails.data && !vm.currentMail) {
+              vm.currentMail = vm.mails.data[0];
+            }
+          } else {
+            console.error('Something went wrong!');
+          }
+
+          vm.loadingMails = false;
+          vm.firstLoad = false;
+        })
+        .catch(e => {
+          vm.loadingMails = false;
+          vm.firstLoad = false;
+
+          console.log(e);
+        });
     },
 
-    methods: {
-        init() {
-            let vm = this;
-
-            vm.loadMails();
-        },
-
-        loadMails() {
-            let vm = this;
-
-            vm.loadingMails = true;
-            axios.get(vm.mails.next_page_url)
-                .then(response => {
-                    let data = response.data;
-
-                    if (data.success) {
-                        let mails = vm.mails.data || [];
-
-                        _.each(data.data.data, (mail) => {
-                            mails.push(mail);
-                        });
-
-                        vm.mails = data.data;
-                        vm.mails.data = mails;
-
-                        if (vm.mails.data && !vm.currentMail) {
-                            vm.currentMail = vm.mails.data[0];
-                        }
-                    } else {
-                        console.error('Something went wrong!');
-                    }
-
-                    vm.loadingMails = false;
-                    vm.firstLoad = false;
-                })
-                .catch(e => {
-                    vm.loadingMails = false;
-                    vm.firstLoad = false;
-
-                    console.log(e);
-                });
-        },
-
-        view(mail) {
-            this.loadingMail = true;
-            this.currentMail = mail;
-        },
-
+    view(mail) {
+      this.loadingMail = true;
+      this.currentMail = mail;
     },
+
+  },
 
 
 })
