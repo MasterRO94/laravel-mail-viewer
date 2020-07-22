6eb007868e7fde683096830cc434760348c69fe7
Roman Ihoshyn <igoshin18@gmail.com>
2020-07-22 19:02:26 +0300

    split view and api call actions

--- src/app/Controllers/MailController.php
+++ src/app/Controllers/MailController.php
@@ -7,31 +7,40 @@ namespace MasterRO\MailViewer\Controllers;
 use Illuminate\Routing\Controller;
 use MasterRO\MailViewer\Models\MailLog;
 
+/**
+ * Class MailController
+ *
+ * @package MasterRO\MailViewer\Controllers
+ */
 class MailController extends Controller
 {
-	/**
-	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-	 */
-	public function index()
-	{
-		$mails = MailLog::latest('date')->paginate(config('mail-viewer.emails_per_page', 20));
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('mail-viewer::mails.index');
+    }
 
-		if (request()->expectsJson()) {
-			return response()->json(['success' => true, 'data' => $mails]);
-		}
+    /**
+     * Emails
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function emails()
+    {
+        $mails = MailLog::latest('date')->paginate(config('mail-viewer.emails_per_page', 20));
 
-		return view('mail-viewer::mails.index', compact('mails'));
-	}
+        return response()->json(['success' => true, 'data' => $mails]);
+    }
 
-
-	/**
-	 * @param MailLog $mailLog
-	 *
-	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-	 */
-	public function show(MailLog $mailLog)
-	{
-		return view('mail-viewer::mails.view', compact('mailLog'));
-	}
-
+    /**
+     * @param MailLog $mailLog
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(MailLog $mailLog)
+    {
+        return view('mail-viewer::mails.view', compact('mailLog'));
+    }
 }
--- src/resources/routes/web.php
+++ src/resources/routes/web.php
@@ -3,13 +3,17 @@
 declare(strict_types=1);
 
 Route::group([
-	'middleware' => config('mail-viewer.middleware', ['web']),
-	'as'         => 'mail-viewer::',
-	'prefix'     => config('grid.uri_prefix', '_mail-viewer'),
+    'middleware' => config('mail-viewer.middleware', ['web']),
+    'as'         => 'mail-viewer::',
+    'prefix'     => config('mail-viewer.uri_prefix', '_mail-viewer'),
 ], function () {
 
-	Route::get('/', '\\MasterRO\\MailViewer\\Controllers\\MailController@index')
-		->name('index');
-	Route::get('/{mailLog}', '\\MasterRO\\MailViewer\\Controllers\\MailController@show')
-		->name('show');
+    Route::get('/', '\\MasterRO\\MailViewer\\Controllers\\MailController@index')
+        ->name('index');
+
+    Route::get('/emails', '\\MasterRO\\MailViewer\\Controllers\\MailController@emails')
+        ->name('emails');
+
+    Route::get('/{mailLog}', '\\MasterRO\\MailViewer\\Controllers\\MailController@show')
+        ->name('show');
 });
