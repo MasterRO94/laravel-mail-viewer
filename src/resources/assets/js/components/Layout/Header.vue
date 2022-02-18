9a83c1fc4ce32b957bb58da1d4841132c853751d
Kirill Zhelebovskiy <kzhelebovskiy@justcoded.co>
2022-02-18 17:31:03 +0200

    Added ability to go to defined site url from header

--- src/resources/assets/js/components/Layout/Header.vue
+++ src/resources/assets/js/components/Layout/Header.vue
@@ -1,25 +1,31 @@
 <template>
   <header class="flex justify-between w-full bg-indigo-500 p-5 rounded-b-md">
-    <div class="flex text-white">
-      <span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="h-12 w-12"
-        >
-          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-          <polyline points="22,6 12,13 2,6" />
-        </svg>
-      </span>
+    <div>
+      <a
+        :href="appUrl"
+        title="Home page"
+        class="flex text-white"
+      >
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-12 w-12"
+          >
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+            <polyline points="22,6 12,13 2,6" />
+          </svg>
+        </span>
 
-      <p class="text-3xl px-3 pt-[0.5rem]">
-        Outbound Emails - <small v-text="appName" />
-      </p>
+        <span class="text-3xl px-3 pt-[0.5rem]">
+          Outbound Emails - <small v-text="appName" />
+        </span>
+      </a>
     </div>
 
     <Stats />
@@ -30,4 +36,5 @@
 import Stats from '../Stats/Stats';
 
 const appName = document.querySelector('#app').dataset.appName;
+const appUrl = document.querySelector('#app').dataset.appUrl;
 </script>
